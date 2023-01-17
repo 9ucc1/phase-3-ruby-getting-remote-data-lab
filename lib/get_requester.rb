396c430966f618801fd2099a60b281c389149ad0
Gucci Fan <gucc@umich.edu>
2023-01-16 21:51:57 -0500

    commit tree 63d789bec2b6bce28bf4cc64314d5863432e087a

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,16 @@
 # Write your code here
+require 'net/http'
+require 'open-uri'
+require 'json'
+
+class GetRequester
+
+    def initialize
+        uri = URI.parse(URL)
+        response = Net::HTTP.get_response(uri)
+        response.body
+    end
+
+    def parse_json
+    end
+end
